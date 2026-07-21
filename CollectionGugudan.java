73f2b257cc18b08ec701a063ab9871d1a9e7bbd0
ckdl6354 <ckdl6354@gmail.com>
2026-07-21 23:32:41 +0900

    구구단의 업그레이드 버전: 구구단 맞추기 퀴즈

--- CollectionGugudan.java
+++ CollectionGugudan.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
-public class Lotto {
+public class CollectionGugudan {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
