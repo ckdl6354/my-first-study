b8a9031c0e651c1701ad3bff8f04fd57bfdee00e
ckdl6354 <ckdl6354@gmail.com>
2026-07-23 23:38:09 +0900

    새 상품 등록 기능 추가

--- MiniProject.java
+++ MiniProject.java
@@ -26,7 +26,7 @@ public class MiniProject {
         // 메인 루프: 1.목록, 2.추가, 3.판매, 4.종료 기능 수행
         while (true) {
             System.out.println("\n--- 마트 재고 관리 시스템 ---");
-            System.out.println("1. 전체 상품 보기 | 2. 재고 추가 | 3. 상품 판매 | 4. 종료");
+            System.out.println("1. 전체 상품 보기 | 2. 재고 추가 | 3. 상품 판매 | 5. 새 상품 등록 | 4. 종료");
             System.out.print("선택: ");
             int menu = sc.nextInt();
             sc.nextLine(); 
@@ -56,12 +56,30 @@ public class MiniProject {
                 } else {
                     System.out.println("판매 불가 (재고 부족 또는 상품 없음)");
                 }
-            } else if (menu == 4) { 
+            } else if (menu == 5) { // 새 상품 등록
+    System.out.print("등록할 새 상품 이름: ");
+    String name = sc.nextLine();
+    
+    // 이미 존재하는 상품인지 확인하는 방어 코드
+    if (mart.containsKey(name)) {
+        System.out.println("이미 등록된 상품입니다. (재고 추가 메뉴를 이용하세요)");
+    } else {
+        System.out.print("상품 가격: ");
+        int price = sc.nextInt();
+        
+        System.out.print("초기 재고 수량: ");
+        int stock = sc.nextInt();
+        sc.nextLine(); // 인트 입력 후 남은 엔터 제거 (버그 방지)
+        
+        // 마트에 새 상품 객체 생성해서 넣기
+        mart.put(name, new Product(name, price, stock));
+        System.out.println("[" + name + "] 상품이 새로 등록되었습니다.");
+    }
+            }else if (menu == 4) { 
                 System.out.println("프로그램을 종료합니다.");
                 break;
             } else { 
                 System.out.println("잘못된 입력입니다.");
-            }
         }
         sc.close();
     }
