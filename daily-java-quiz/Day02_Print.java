c2c48895d367c9b3e9ed0151e804b1fdd0d3750d
ckdl6354 <ckdl6354@gmail.com>
2026-08-02 00:03:09 +0900

    간단한 출력 문제 해결

--- daily-java-quiz/Day02_Print.java
+++ daily-java-quiz/Day02_Print.java
@@ -2,29 +2,29 @@
 
 public class Day02_Print {
 	public static void main(String[] args) {
- 	int a=10;
+ 	
+	// --- Jol9009번 문제 ---	
+	int a=10;
  	String b="A";
  	System.out.println("a = "+a);
  	System.out.println("b = "+b);
- 	
-	}
-}
-public class Day2_Print {
-	public static void main(String[] args) {
+
+    System.out.println("--------------------");
+
+    // --- Jol9010번 문제 ---
  	int a=10,b=20;
  	System.out.println(b+" "+a);	
-	}
-}
-public class Day2_Print {
-	public static void main(String[] args) {
+		
+    System.out.println("--------------------");
+	
+	// --- Jol9011번 문제 ---	
  	int a=10,b=20;
  	System.out.printf("%d + %d = %d\n",a,b,a+b);
  	System.out.printf("%d + %d = %d",a+20,b+20,a+20+b+20);
-	}
-}
-package com.test;
-public class Day1Coding {
-	public static void main(String[] args) {
+
+	System.out.println("--------------------");
+
+	// --- Jol511번 문제 ---	
  	int a=55,b=10;
  	System.out.printf("%d - %d = %d\n",a,b,a-b);
  	System.out.printf("%d - %d = %d",a+1953,b+1989,a-b-36);
