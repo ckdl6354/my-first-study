9c8f21d90e6dd3fc5fd2913824268d7a96ed6b9e
ckdl6354 <ckdl6354@gmail.com>
2026-08-01 23:30:49 +0900

    간단한 출력 문제

---
+++ daily-java-quiz/Day02_Print.java
@@ -0,0 +1,13 @@
+// https://jungol.co.kr/ Jol9009번 문제
+package com.test;
+public class Day02_Print {
+	public static void main(String[] args) {
+ 	int a=10;
+ 	String b="A";
+ 	System.out.println("a = "+a);
+ 	System.out.println("b = "+b);
+ 	
+	}
+}
+
+
