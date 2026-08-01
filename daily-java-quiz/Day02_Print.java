fd8712ce15ef40ac850f0460154ccf2ad0ca988d
ckdl6354 <ckdl6354@gmail.com>
2026-08-01 23:54:05 +0900

    간단한 출력 문제 해결

--- daily-java-quiz/Day02_Print.java
+++ daily-java-quiz/Day02_Print.java
@@ -1,4 +1,4 @@
-// 출처:https://jungol.co.kr/ Jol9009번,9010번 문제
+// 출처:https://jungol.co.kr/ Jol9009번,9010번,9011번 문제
 
 public class Day02_Print {
 	public static void main(String[] args) {
@@ -9,11 +9,17 @@ public class Day02_Print {
  	
 	}
 }
-
 public class Day2_Print {
 	public static void main(String[] args) {
  	int a=10,b=20;
  	System.out.println(b+" "+a);	
+	}
+}
+public class Day2_Print {
+	public static void main(String[] args) {
+ 	int a=10,b=20;
+ 	System.out.printf("%d + %d = %d\n",a,b,a+b);
+ 	System.out.printf("%d + %d = %d",a+20,b+20,a+20+b+20);
 	}
 }
 
