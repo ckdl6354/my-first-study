e04702c400a48a2f413a53c8580922d345d962ad
ckdl6354 <ckdl6354@gmail.com>
2026-08-05 00:02:07 +0900

    자바 배열 문제 해결

--- daily-java-quiz/Day04_Array.java
+++ daily-java-quiz/Day04_Array.java
@@ -5,12 +5,12 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Day4_Array{
-	public static void main(String[] args) {
+        public class Day4_Array{
+	    public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int[] arr = new int[11];
 	
-	while(true) {
+	    while(true) {
 		int num =sc.nextInt();
 		
 		if(num < 1 || num > 10) {
@@ -45,7 +45,7 @@ public class Day4_Array{
 	}
 	//Jol 9066번 문제
 
-char [] ch = new char [26];
+        char [] ch = new char [26];
 		char input='Z';
 		for(int i =0;i<ch.length;i++) {
 			ch[i] = input;
