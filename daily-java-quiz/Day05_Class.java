24919d5f5eab2cf9b5323e1e25c0d5de2aaf3de8
ckdl6354 <ckdl6354@gmail.com>
2026-08-05 23:42:53 +0900

    자바 구조체 문제 해결

--- daily-java-quiz/Day05_Class.java
+++ daily-java-quiz/Day05_Class.java
@@ -20,3 +20,20 @@ public class Day1Coding {
 	System.out.printf("당신의 이름은 %s이고 나이는%d이세이군요.\n",p.name,p.age);
 	}
 }
+//Jol 613번 문제 해결
+public class Day1Coding {
+static class Person{
+	String Name,School;
+	int Grade;
+}
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	Person p = new Person();
+	p.Name = sc.next();	
+	p.School = sc.next();
+	p.Grade = sc.nextInt();
+	System.out.printf("Name : %s \nSchool : %s \nGrade : %d",p.Name,p.School,p.Grade);
+	}
+}
+
+
