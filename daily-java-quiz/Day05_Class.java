724fff4d0148f81896c98b876af1e3a5db20eaf2
ckdl6354 <ckdl6354@gmail.com>
2026-08-06 23:31:51 +0900

    자바 구조체 문제 해결

--- daily-java-quiz/Day05_Class.java
+++ daily-java-quiz/Day05_Class.java
@@ -5,63 +5,63 @@
 package com.test;
 import java.util.Scanner;
 public class Day1Coding {
-	static class Person{
-		String name;
-		int age;
-	}
+static class Person{
+	String name;
+	int age;
+}
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+Person p = new Person();
+
+System.out.println("당신의 이름은 무엇입니까? ");
+p.name = sc.next();
+System.out.println("당신의 나이는 몇살입니까?");
+p.age = sc.nextInt();
+System.out.printf("당신의 이름은 %s이고 나이는%d이세이군요.\n",p.name,p.age);
+}
+}
+//Jol 613번 문제 해결
+	class Person {
+	String Name;
+	String School;
+	int Grade;
+}
+	public class Day1Coding {
 	public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	Person p = new Person();
-	
-	System.out.println("당신의 이름은 무엇입니까? ");
-	p.name = sc.next();
-	System.out.println("당신의 나이는 몇살입니까?");
-	p.age = sc.nextInt();
-	System.out.printf("당신의 이름은 %s이고 나이는%d이세이군요.\n",p.name,p.age);
-	}
-}
-//Jol 613번 문제 해결
-        class Person {
-	    String Name;
-	    String School;
-	    int Grade;
-}
-        public class Day1Coding {
-        public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Person p = new Person();
 
-		String Name = sc.next();
-		String School = sc.next();
-		int Grade = sc.nextInt();
-		System.out.printf("Name : %s\nSchool : %s\nGrade : %d", Name, School, Grade);
-	}
+	String Name = sc.next();
+	String School = sc.next();
+	int Grade = sc.nextInt();
+	System.out.printf("Name : %s\nSchool : %s\nGrade : %d", Name, School, Grade);
+}
 }
 
 
 //Jol 9131번 문제 해결
 import java.util.Scanner;
 class Person {
-	String Name;
-	int age;
+String Name;
+int age;
 {Name = "손예준";
-	age=12;
+age=12;
 }
 }
 public class Day1Coding {
 public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Person p = new Person();
+	Scanner sc = new Scanner(System.in);
+	Person p = new Person();
 
-	    String Name = p.Name;
-		int age = p.age;
-		
-		System.out.println("친한 친구의 이름과 나이를 입력하세요. ");
-		String Name1 = sc.next();
-		int age1 = sc.nextInt();
-		
-		System.out.printf("당신의 이름 : %s, 나이 : %d\n",Name,age);
-		System.out.printf("친구의 이름 : %s, 나이 : %d",Name1,age1);
+	String Name = p.Name;
+	int age = p.age;
+	
+	System.out.println("친한 친구의 이름과 나이를 입력하세요. ");
+	String Name1 = sc.next();
+	int age1 = sc.nextInt();
+	
+	System.out.printf("당신의 이름 : %s, 나이 : %d\n",Name,age);
+	System.out.printf("친구의 이름 : %s, 나이 : %d",Name1,age1);
 }
 }
 
