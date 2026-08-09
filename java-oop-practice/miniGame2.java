08fddbd01cb7d50f1cb509e892bc6e7beff1b147
ckdl6354 <ckdl6354@gmail.com>
2026-08-10 00:09:25 +0900

    턴제식 게임 구현

--- java-oop-practice/miniGame2.java
+++ java-oop-practice/miniGame2.java
@@ -58,6 +58,12 @@ class Hero { // 모양과 기능을 가진 설계도 (변수)
 				if(num == 1){
 					System.out.println("몬스터를 발견했습니다!);
 				}
+				if(num==2) {
+				System.out.println("체력(20)이 회복되었습니다.");
+				myHero.hp+= 20;
+				System.out.println(myHero.name+"의 체력: "+myHero.hp);
+				continue;
+			}
 				myHero.attack(boss);
 				if (boss.hp <= 0) {
 					System.out.println(boss.name + "를 물리쳤습니다.");
@@ -71,7 +77,8 @@ class Hero { // 모양과 기능을 가진 설계도 (변수)
 					break;
 
 				}
-				
+			    System.out.println(myHero.name+"의 남은 체력: "+myHero.hp);
+
 			}
 		}
 	}
