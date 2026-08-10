12385b8944ba570b19de3f3cb566a203a66bc516
ckdl6354 <ckdl6354@gmail.com>
2026-08-10 23:22:44 +0900

    자바 배열 문제 해결

--- daily-java-quiz/Day04_Array.java
+++ daily-java-quiz/Day04_Array.java
@@ -56,5 +56,19 @@ import java.util.Scanner;
 		}
 	}
 }
+public class Day04_Array {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[5];
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+			if (i == 0 || i == 2 || i == 4) {
+				sum += arr[i];				
+			}
+		}
+		System.out.print(sum);
+	}
+}
 		
 }
