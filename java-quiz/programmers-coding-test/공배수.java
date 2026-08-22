920833051bf18331ee1d1d8d0301054325562388
ckdl6354 <ckdl6354@gmail.com>
2026-08-22 23:30:51 +0900

    공배수 문제풀이

--- java-quiz/programmers-coding-test/공배수.java
+++ java-quiz/programmers-coding-test/공배수.java
@@ -1,6 +1,6 @@
 //문제링크 https://school.programmers.co.kr/learn/courses/30/lessons/181936
 
-class 공배수.java {
+class 공배수{
     public int solution(int number, int n, int m) {
         if(number%n==0 && number%m  == 0 ){
             return 1;
